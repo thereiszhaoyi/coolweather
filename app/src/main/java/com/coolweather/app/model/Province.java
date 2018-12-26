8ddc16b6c0bd5159d33a06bf4ee86ef59c9167e8
zhaoyi <43136799+thereiszhaoyi@users.noreply.github.com>
2018-12-26 16:41:01 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,33 @@
+package com.coolweather.app.model;
+
+
+
+public class Province {
+    private int id;//每个实体类中都应该有的字段
+    private String provinceName;//省的名字
+    private String provinceCode;//省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
